5999c3a8a1d1ecf13497ca15fc9a79bcfbd52031
Noel Welsh <noelwelsh@gmail.com>
2011-06-20 16:57:10 +0100

    Fix tests to work with type-safe URL patterns.

--- routes/src/test/scala/bigtop/routes/PatternSuite.scala
+++ routes/src/test/scala/bigtop/routes/PatternSuite.scala
@@ -3,79 +3,97 @@ package routes
 
 import org.scalatest._
 
+import PatternOps._
+
 class PatternSuite extends FunSuite with Assertions {
 
-  test("/") {
-    val p = NilPattern
+  def expectTuple[A, B](v1: Tuple1[A], v2: Tuple1[B]) =
+    expect(v1)(v2)
+  def expectTuple[A,B](v1: Tuple2[A,B], v2: Tuple2[A,B]) =
+    expect(v1)(v2)
+  def expectTuple[A,B,C](v1: Tuple3[A,B,C], v2: Tuple3[A,B,C]) =
+    expect(v1)(v2)
+  def expectTuple[A,B,C,D](v1: Tuple4[A,B,C,D], v2: Tuple4[A,B,C,D]) =
+    expect(v1)(v2)
 
-    expect(Some(()))(p decode Nil)
+
+  test("/") {
+    val p = PNil
+
+    expect(Some(HNil))(p decode Nil)
     expect(None)(p decode List(""))
   }
 
   test("/ const") {
-    val p = NilPattern / "abc"
-    expect(Some(()))(p decode List("abc"))
-    expect(None    )(p decode List("ab"))
-    expect(None    )(p decode List("abcd"))
-    expect(None    )(p decode Nil)
+    val p = "abc" /: PNil
+    expect(Some(HNil))(p decode List("abc"))
+    expect(None      )(p decode List("ab"))
+    expect(None      )(p decode List("abcd"))
+    expect(None      )(p decode Nil)
   }
 
   test("/ arg") {
-    val p = NilPattern / IntArg
-    expect(Some(Tuple1(123)))(p decode List("123"))
-    expect(None       )(p decode List("abc"))
-    expect(None       )(p decode Nil)
+    val p = IntArg /: PNil
+    expectTuple(Tuple1(123), (p decode List("123")).get)
+
+    expect(None)(p decode List("abc"))
+    expect(None)(p decode Nil)
   }
 
   test("/ const / const") {
-    val p = NilPattern / "abc" / "def"
-    expect(Some(()))(p decode List("abc", "def"))
-    expect(None    )(p decode List("def", "abc"))
+    val p = "abc" /: "def" /: PNil
+    expect(Some(HNil))(p decode List("abc", "def"))
+    expect(None      )(p decode List("def", "abc"))
   }
 
   test("/ const / arg") {
-    val p = NilPattern / "abc" / IntArg
-    expect(Some(Tuple1(123)))(p decode List("abc", "123"))
-    expect(None             )(p decode List("abc", "def"))
+    val p = "abc" /: IntArg /: PNil
+    expectTuple(Tuple1(123), (p decode List("abc", "123")).get)
+
+    expect(None)(p decode List("abc", "def"))
   }
 
   test("/ arg / const") {
-    val p = NilPattern / IntArg / "def"
-    expect(Some(Tuple1(123)))(p decode List("123", "def"))
-    expect(None             )(p decode List("abc", "def"))
+    val p = "def" /: IntArg /: PNil
+    expectTuple(Tuple1(123), (p decode List("def", "123")).get)
+
+    expect(None)(p decode List("abc", "def"))
   }
 
   test("/ arg / arg") {
-    val p = NilPattern / IntArg / IntArg
-    expect(Some((123, 456)))(p decode List("123", "456"))
-    expect(Some((456, 123)))(p decode List("456", "123"))
-    expect(None            )(p decode List("123", "abc"))
+    val p = IntArg /: IntArg /: PNil
+    expectTuple((123, 456), (p decode List("123", "456")).get)
+    expectTuple((456, 123), (p decode List("456", "123")).get)
+
+    expect(None)(p decode List("123", "abc"))
   }
 
   test("/ arg / arg / const") {
-    val p = NilPattern / IntArg / StringArg / "abc"
-    expect(Some((123, "456")))(p decode List("123", "456", "abc"))
-    expect(Some((456, "123")))(p decode List("456", "123", "abc"))
-    expect(None              )(p decode List("abc", "456", "abc"))
+    val p = IntArg /: StringArg /: "abc" /: PNil
+    expectTuple((123, "456"), (p decode List("123", "456", "abc")).get)
+    expectTuple((456, "123"), (p decode List("456", "123", "abc")).get)
+
+    expect(None)(p decode List("abc", "456", "abc"))
   }
 
   test("/ arg / arg / arg") {
-    val p = NilPattern / IntArg / StringArg / IntArg
-    expect(Some((123, "456", 789)))(p decode List("123", "456", "789"))
-    expect(Some((789, "456", 123)))(p decode List("789", "456", "123"))
-    expect(None                   )(p decode List("123", "456"))
+    val p = IntArg /: StringArg /: IntArg /: PNil
+    expectTuple((123, "456", 789), (p decode List("123", "456", "789")).get)
+    expectTuple((789, "456", 123), (p decode List("789", "456", "123")).get)
+
+    expect(None)(p decode List("123", "456"))
   }
     
   test("/ arg / arg / arg / const") {
-    val p = NilPattern / IntArg / StringArg / IntArg / "ghi"
-    expect(Some((123, "456", 789)))(p decode List("123", "456", "789", "ghi"))
-    expect(None                   )(p decode List("abc", "456", "789", "ghi"))
+    val p = IntArg /: StringArg /: IntArg /: "ghi" /: PNil
+    expectTuple((123, "456", 789), (p decode List("123", "456", "789", "ghi")).get)
+    expect(None)(p decode List("abc", "456", "789", "ghi"))
   }
 
   test("/ arg / arg / arg / arg") {
-    val p = NilPattern / IntArg / StringArg / IntArg / StringArg
-    expect(Some((123, "456", 789, "012")))(p decode List("123", "456", "789", "012"))
-    expect(Some(( 12, "789", 456, "123")))(p decode List("012", "789", "456", "123"))
+    val p = IntArg /: StringArg /: IntArg /: StringArg /: PNil
+    expectTuple((123, "456", 789, "012"), (p decode List("123", "456", "789", "012")).get)
+    expectTuple(( 12, "789", 456, "123"), (p decode List("012", "789", "456", "123")).get)
   }
   
 }
