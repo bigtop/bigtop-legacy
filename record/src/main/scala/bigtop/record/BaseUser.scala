eb95b89ef0a606e055098c32944736e81e567d4b
Dave Gurnell <dave@untyped.com>
2013-03-04 11:16:04 +0000

    Soften regex on email addresses in Bigtop User

--- record/src/main/scala/bigtop/record/BaseUser.scala
+++ record/src/main/scala/bigtop/record/BaseUser.scala
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright 2011 Untyped Ltd
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@
 package bigtop
 package record
 
+import java.util.regex._
 import scala.xml._
 
 import net.liftweb.common._
@@ -33,30 +34,30 @@ import net.liftweb.util.Mailer._
  */
 trait BaseUser[T <: BaseUser[T]] extends Record[T] with Loggable {
   self: T =>
-  
+
   protected def baseUserMeta =
     meta.asInstanceOf[BaseUserMeta[T]]
-  
+
   def save: T
-  
+
   def delete: T
-  
+
   val username = new StringField(this, 256) {
     override def setFilter =
       trim _ ::
       super.setFilter
-    
+
     override def validations =
       valMinLen(1, "Please enter a username") _ ::
       valUnique("That username is already taken") _ ::
       super.validations
-    
+
     def valUnique(msg: String)(username: String): List[FieldError] =
       if(usernameTaken(username))
-	List(FieldError(this, msg))
+        List(FieldError(this, msg))
       else
-	Nil
-				      
+        Nil
+
   }
 
   // bcrypt encrypted password:
@@ -64,7 +65,7 @@ trait BaseUser[T <: BaseUser[T]] extends Record[T] with Loggable {
     override def validations =
       valPasswordValid _ ::
       super.validations
-    
+
     def valPasswordValid(str: String): List[FieldError] =
       if(baseUserMeta.isValidPassword(str)) {
         Nil
@@ -72,20 +73,30 @@ trait BaseUser[T <: BaseUser[T]] extends Record[T] with Loggable {
         List(FieldError(this, baseUserMeta.invalidPasswordMessage))
       }
   }
-  
+
   val email = new EmailField(this, 256) {
+    val emailRx = Pattern.compile("^[^@]+@[^@]+$")
+
     override def setFilter =
       trim _ ::
       super.setFilter
-    
+
+    def validateEmail(email: String): List[FieldError] =
+      Option(email) match {
+        case Some(email) if emailRx.matcher(email).matches => Nil
+        case _ => Text("Please enter a valid email address")
+
+      }
+
     override def validations =
       valMinLen(1, "Please enter an email address") _ ::
-      super.validations
+      validateEmail _ ::
+      Nil
   }
-  
+
   /**
    * Unique code used to verify the user's email address.
-   * 
+   *
    * A new code is generated when the user signs up or clicks "forgot password".
    * The code is placed into an email verification URL, which is then hooked up
    * to a piece of code that sets user.emailVerified to true and clears the code.
@@ -94,13 +105,13 @@ trait BaseUser[T <: BaseUser[T]] extends Record[T] with Loggable {
    * The programmer must implement these using the (simple) snippets provided.
    */
   val emailVerificationCode = new OptionalStringField(this, 28)
-  
+
   /**
    * Has the user's email address been verified?
    * See emailVerificationCode for more information.
    */
   val emailVerified = new BooleanField(this)
-  
+
   /**
    * Returns `true` if another user (not this user) has the specified `username`.
    */
@@ -108,23 +119,23 @@ trait BaseUser[T <: BaseUser[T]] extends Record[T] with Loggable {
 
   /**
    * Is this user allowed to log in?
-   * 
-   * The default implementation returns true if the user's email address 
+   *
+   * The default implementation returns true if the user's email address
    * has been verified. See emailVerificationCode for more information.
    */
   def canLogIn = emailVerified.is
-  
+
   /** Is this user allowed to change identity? False by default. */
   def canChangeIdentity: Boolean = false
-  
+
   def makeEmailVerificationCode: String =
     Helpers.nextFuncName
-  
+
   def sendSignUpEmail = {
     val code = makeEmailVerificationCode
-    
+
     emailVerificationCode(Some(code)).emailVerified(false).save
-    
+
     sendEmail(
       "signup email",
       signUpEmailFrom,
@@ -137,22 +148,22 @@ trait BaseUser[T <: BaseUser[T]] extends Record[T] with Loggable {
   def signUpEmailTo: To = To(email.is)
   def signUpEmailSubject: Subject = Subject("Welcome")
   def signUpEmailUrl(code: String) = "/reset-password?code=" + code
-  
+
   def signUpEmailBody(url: String): MailBodyType =
     XHTMLMailBodyType(
       <body>
         <p>Please click on the link below to activate your account:</p>
-      
+
         <p><strong>&nbsp;&nbsp;<a href={url}>{url}</a></strong></p>
-      
+
         <p>Once you have done this you will be able to log in.</p>
-      
+
         <p style="font-size: .9em">(Please do not reply to this message.)</p>
       </body>)
 
   def sendForgotPasswordEmail = {
     val code = makeEmailVerificationCode
-    
+
     emailVerificationCode(Some(code)).save
     sendEmail(
       "forgot password",
@@ -161,24 +172,24 @@ trait BaseUser[T <: BaseUser[T]] extends Record[T] with Loggable {
       forgotPasswordEmailTo,
       forgotPasswordEmailBody(forgotPasswordEmailUrl(code)))
   }
-  
+
   def forgotPasswordEmailFrom: From = From("noreply@example.com")
   def forgotPasswordEmailTo: To = To(email.is)
   def forgotPasswordEmailSubject: Subject = Subject("Forgot password")
   def forgotPasswordEmailUrl(code: String) = "/reset-password?code=" + code
-  
+
   def forgotPasswordEmailBody(url: String): MailBodyType =
     XHTMLMailBodyType(
       <body>
         <p>Please click on the link below to reset your password:</p>
-        
+
         <p><a href={url}>{url}</a></p>
-        
+
         <p>Please disregard this message if you believe you have been sent it incorrectly.</p>
-        
+
         <p style="font-size: .9em">(Please do not reply to this message.)</p>
       </body>)
-  
+
   def sendEmail(desc: String, from: From, subject: Subject, to: To, body: MailBodyType) = {
     logger.info("Sending " + desc + " email:\n" + from + "\n" + subject +  "\n" + to + "\n" + body)
     Mailer.sendMail(from, subject, to, body)
@@ -192,45 +203,45 @@ trait BaseUser[T <: BaseUser[T]] extends Record[T] with Loggable {
  */
 trait BaseUserMeta[T <: BaseUser[T]] {
   self: T =>
-  
+
   // Real identity ------------------------------
-  
+
   private object realUsernameVar extends SessionVar[Option[String]](Empty)
   private object realUserVar extends RequestVar[Option[T]](
     realUsernameVar.is.flatMap(byUsername _))
 
   def realUsername = realUsernameVar.is
   def realUser = realUserVar.is
-  
+
   // Effective identity -------------------------
-  
+
   private object effectiveUsernameVar extends SessionVar[Option[String]](Empty)
   private object effectiveUserVar extends RequestVar[Option[T]](
     effectiveUsernameVar.is.flatMap(byUsername _))
 
   def effectiveUsername = effectiveUsernameVar.is
   def effectiveUser = effectiveUserVar.is
-  
+
   // User queries -------------------------------
-  
+
   /**
    * Retrieve a user from the database by username. Override this to:
    *  - implement for various database backends;
    *  - use email addresses as usernames, or use separate usernames.
    *
-   * Note the deviation from standard nomenclature. 
+   * Note the deviation from standard nomenclature.
    * Most byFoo methods return query objects. We can't do that here because
-   * we don't know what backend we're using. We therefore return an option instead. 
+   * we don't know what backend we're using. We therefore return an option instead.
    */
   def byUsername(username: String): Option[T]
-  
+
   /**
    * Retrieve a user from the database by email address. Override this to
    * implement for various backends.
    *
-   * Note the deviation from standard nomenclature. 
+   * Note the deviation from standard nomenclature.
    * Most byFoo methods return query objects. We can't do that here because
-   * we don't know what backend we're using. We therefore return an option instead. 
+   * we don't know what backend we're using. We therefore return an option instead.
    */
   def byEmail(email: String): Option[T]
 
@@ -238,9 +249,9 @@ trait BaseUserMeta[T <: BaseUser[T]] {
    * Retrieve a user from the database by email verification code. Override this to
    * implement for various database backends.
    *
-   * Note the deviation from standard nomenclature. 
+   * Note the deviation from standard nomenclature.
    * Most byFoo methods return query objects. We can't do that here because
-   * we don't know what backend we're using. We therefore return an option instead. 
+   * we don't know what backend we're using. We therefore return an option instead.
    */
   def byEmailVerificationCode(code: String): Option[T]
 
@@ -249,17 +260,17 @@ trait BaseUserMeta[T <: BaseUser[T]] {
    * Return Some(user) if the username and password are both correct, None
    * otherwise.
    *
-   * Note the deviation from standard nomenclature. 
+   * Note the deviation from standard nomenclature.
    * Most byFoo methods return query objects. We can't do that here because:
    *  - we don't know what backend we're using;
    *  - we have to do the password check outside of the DB.
-   * We therefore return an option instead. 
+   * We therefore return an option instead.
    */
   def byUsernameAndPassword(username: String, password: String): Option[T] =
     byUsername(username).filter(_.password.match_?(password))
-  
+
   // Password validation ------------------------
-  
+
   /**
    * Does the supplied password meet the application's strength requirements for passwords?
    *
@@ -267,7 +278,7 @@ trait BaseUserMeta[T <: BaseUser[T]] {
    */
   def isValidPassword(password: String) =
     password.length >= 6
-  
+
   /**
    * A message describing the application's strength requirements for passwords.
    *
@@ -277,21 +288,21 @@ trait BaseUserMeta[T <: BaseUser[T]] {
     "Please enter six characters or more"
 
   // Log in, log out ----------------------------
-  
+
   /** Is the user's effective identity logged in? */
   def isLoggedIn: Boolean =
     effectiveUsername.isDefined
-  
+
   /** Is the user's real identity logged in? */
   def isReallyLoggedIn: Boolean =
     realUsername.isDefined
-  
-  /** 
+
+  /**
    * Log the user in. Return true if successful, false if the username/password was wrong.
    * Does not inform the user in any way.
-   * 
+   *
    * Both real and effective identities are set to the relevant username
-   * (and user if the username is valid). If a user is already logged in, 
+   * (and user if the username is valid). If a user is already logged in,
    * their real and effective identities are replaced by this new identity.
    */
   def logIn(username: String, password: String): Boolean =
@@ -300,53 +311,53 @@ trait BaseUserMeta[T <: BaseUser[T]] {
     filter(user => user.canLogIn).
     map(user => { logIn(user); true }).
     getOrElse(false)
-  
-  /** 
+
+  /**
    * Log the user in. Return true if successful, false if the username/password was wrong.
    * Does not inform the user in any way.
-   * 
+   *
    * On success, both real and effective identities are set to the relevant user.
    * If a user is already logged in, their real and effective identities are replaced
    * by this new identity.
    */
   def logIn(user: T): Unit =
     logIn(user.username.is)
-  
-  /** 
+
+  /**
    * Log the user in.
-   * 
+   *
    * Both real and effective identities are set to the relevant username
-   * (and user if the username is valid). If a user is already logged in, 
+   * (and user if the username is valid). If a user is already logged in,
    * their real and effective identities are replaced by this new identity.
    */
   def logIn(username: String): Unit = {
     realUserVar.remove
     effectiveUserVar.remove
-    
+
     realUsernameVar.set(Some(username))
     effectiveUsernameVar.set(Some(username))
   }
-  
-  /** 
+
+  /**
    * Log the user out. Real and effective identities are both set to None.
-   * 
-   * onLogOut callbacks are invoked afterwards, regardless of the 
+   *
+   * onLogOut callbacks are invoked afterwards, regardless of the
    * success/failure of the login attempt. Override these to inform the user.
    */
   def logOut: Unit = {
     val real = realUser
     val effective = effectiveUser
-    
+
     realUsernameVar.remove
     realUserVar.remove
     effectiveUsernameVar.remove
     effectiveUserVar.remove
   }
-  
+
   // Change identity, restore identity ----------
-  
+
   /**
-   * If the User.realUser.canChangeIdentity, 
+   * If the User.realUser.canChangeIdentity,
    * change the user's effective identity to the supplied user.
    * The user's real identity always remains the same.
    *
@@ -358,14 +369,14 @@ trait BaseUserMeta[T <: BaseUser[T]] {
         effectiveUserVar.remove
         effectiveUsernameVar.set(Full(user.username.is))
         true
-      
+
       case _ => false
     }
-   
+
   /** (Re)sets the user's effective identity to be the same as their real identity. */
   def restoreIdentity: Unit = {
     effectiveUserVar.remove
     effectiveUsernameVar.set(realUsernameVar.get)
   }
-  
+
 }
